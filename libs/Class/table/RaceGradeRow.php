04f1260a399e83777b610081560c5d6c5601879a
polaris80red <polaris8000red@gmail.com>
2025-08-20 20:07:15 +0900

    RaceGradeRowクラスの機能をTableRowクラスに分割

--- libs/Class/table/RaceGradeRow.php
+++ libs/Class/table/RaceGradeRow.php
@@ -1,5 +1,5 @@
 <?php
-class RaceGradeRow {
+class RaceGradeRow extends TableRow {
     public const INT_COLUMNS=[
         'id',
         'sort_number',
@@ -22,17 +22,4 @@ class RaceGradeRow {
     public $sort_number;
     public $show_in_select_box=1;
     public $is_enabled=1;
-    /**
-     * 行相当の配列からこのオブジェクトにセットする
-     */
-    public function setFromArray(array $input_row){
-        $columns=self::getRowNames();
-        foreach($columns as $col){
-            $this->{$col}=$input_row[$col];
-        }
-        return $this;
-    }
-    public static function getRowNames(){
-        return array_merge(self::INT_COLUMNS,self::STR_COLUMNS);
-    }
 }
---
+++ libs/Class/table/TableRow.php
@@ -0,0 +1,18 @@
+<?php
+abstract class TableRow {
+    public const INT_COLUMNS=[];
+    public const STR_COLUMNS=[];
+    /**
+     * 行相当の配列からこのオブジェクトにセットする
+     */
+    public function setFromArray(array $input_data){
+        $columns=static::getRowNames();
+        foreach($columns as $col){
+            $this->{$col}=$input_data[$col];
+        }
+        return $this;
+    }
+    public static function getRowNames(){
+        return array_merge(static::INT_COLUMNS,static::STR_COLUMNS);
+    }
+}
