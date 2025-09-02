01b477372c8abd3bb74c80d1dfb0916af6fe76f6
polaris80red <polaris8000red@gmail.com>
2025-09-02 19:36:44 +0900

    fix:メソッド名の誤記修正

--- libs/Class/table/TableRow.php
+++ libs/Class/table/TableRow.php
@@ -6,13 +6,13 @@ abstract class TableRow {
      * 行相当の配列からこのオブジェクトにセットする
      */
     public function setFromArray(array $input_data){
-        $columns=static::getRowNames();
+        $columns=static::getColumnNames();
         foreach($columns as $col){
             $this->{$col}=$input_data[$col];
         }
         return $this;
     }
-    public static function getRowNames(){
+    public static function getColumnNames(){
         return array_merge(static::INT_COLUMNS,static::STR_COLUMNS);
     }
 }
