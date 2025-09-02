d2cecfb92f284b13a007e7aee978cfd1ad4cfa76
polaris80red <polaris8000red@gmail.com>
2025-09-02 19:38:09 +0900

    update: TableRowクラスのカラム取得の機能追加

--- libs/Class/table/TableRow.php
+++ libs/Class/table/TableRow.php
@@ -12,7 +12,42 @@ abstract class TableRow {
         }
         return $this;
     }
-    public static function getColumnNames(){
-        return array_merge(static::INT_COLUMNS,static::STR_COLUMNS);
+    /**
+     * カラム名を配列で取得
+     * @param string|array $exclude 除外するカラム名またはカラム名のリスト
+     */
+    public static function getColumnNames(string|array $exclude=''){
+        $result=array_merge(static::INT_COLUMNS,static::STR_COLUMNS);
+        return self::removeColumns($result,$exclude);
+    }
+    /**
+     * 文字列型カラムの名前のリストを配列で取得
+     * @param string|array $exclude 除外するカラム名またはカラム名のリスト
+     */
+    public static function getStrColmunNames(string|array $exclude=''):array {
+        return self::removeColumns(static::STR_COLUMNS,$exclude);
+    }
+    /**
+     * 整数型カラムの名前のリストを配列で取得
+     * @param string|array $exclude 除外するカラム名またはカラム名のリスト
+     */
+    public static function getIntColmunNames(string|array $exclude=''):array {
+        return self::removeColumns(static::INT_COLUMNS,$exclude);
+    }
+    /**
+     * カラム名の配列から、名前またはリストで指定したカラム名を除去
+     * @param string|array $exclude 除外するカラム名またはカラム名のリスト
+     */
+    private static function removeColumns($column_names,string|array $exclude=''){
+        if($exclude===''){
+            return $column_names;
+        }
+        if($exclude===[]){
+            return $column_names;
+        }
+        if(is_array($exclude)){
+            return array_diff($column_names,$exclude);
+        }
+        return array_diff($column_names,[$exclude]);
     }
 }
