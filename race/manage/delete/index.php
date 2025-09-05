9c9a74d738f0b47d17ad0441400414de9ae48958
polaris80red <polaris8000red@gmail.com>
2025-09-05 19:48:52 +0900

    update: レース情報の削除の確認は個別結果削除と同様にタイトルを赤くする

--- race/manage/delete/index.php
+++ race/manage/delete/index.php
@@ -49,7 +49,7 @@ if($page->error_exists){
 <body>
 <header>
 <?php $page->printHeaderNavigation(); ?>
-<h1 class="page_title"><?=h($page->title)?></h1>
+<h1 class="page_title" style="color:red;"><?=h($page->title)?></h1>
 </header>
 <main id="content">
 <hr class="no-css-fallback">
