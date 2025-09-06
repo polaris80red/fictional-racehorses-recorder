c41abd04b3a57246b4d3495cd737f4b68c303cd6
polaris80red <polaris8000red@gmail.com>
2025-09-06 18:04:14 +0900

    update: 差し込みテンプレート内からページ情報のインスタンスを読み取れるよう引数に追加

--- libs/Class/TemplateImporter.php
+++ libs/Class/TemplateImporter.php
@@ -14,16 +14,16 @@ class TemplateImporter {
     public static function setUserDir(string $userDir){
         self::$userDir=$userDir;
     }
-    public static function include(string $templatePath){
-        if(self::$userDir!=='' && self::includeIfExists(self::$userDir.'/'.$templatePath)){
+    public static function include(string $templatePath, Page|null $page = null){
+        if(self::$userDir!=='' && self::includeIfExists(self::$userDir.'/'.$templatePath,$page)){
             return true;
         }
-        if(self::$themeDir!=='' && self::includeIfExists(self::$themeDir.'/'.$templatePath)){
+        if(self::$themeDir!=='' && self::includeIfExists(self::$themeDir.'/'.$templatePath,$page)){
             return true;
         }
-        return self::includeIfExists(self::$defaultDir.'/'.$templatePath);
+        return self::includeIfExists(self::$defaultDir.'/'.$templatePath,$page);
     }
-    private static function includeIfExists($path){
+    private static function includeIfExists($path, Page|null $page = null){
         if(file_exists($path)){
             include $path;
             return true;
