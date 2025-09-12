4f63c1864f6cad17bdd9559db8c0378210fb60a2
polaris80red <polaris8000red@gmail.com>
2025-09-12 19:22:16 +0900

    change: 管理画面トップの項目の並び順など調整

--- admin/index.php
+++ admin/index.php
@@ -39,7 +39,10 @@ if(!$session->is_logined()){
     </tr>
     <tr>
         <td><?=(new MkTagA("表示設定",InAppUrl::to('setting/')));?></td>
-        <td>表示対象のワールドや年の範囲・日付形式などを設定します</td>
+        <td>どのワールドを表示するかや、年の範囲・日付形式などを設定します</td>
+    </tr>
+    <tr>
+        <td colspan="2"></td>
     </tr>
     <tr>
         <td><?=(new MkTagA("ワールド設定",InAppUrl::to('admin/world/list.php')));?></td>
@@ -53,8 +56,11 @@ if(!$session->is_logined()){
         <td colspan="2"></td>
     </tr>
     <tr>
-        <td><?=(new MkTagA("競馬場マスタ管理",InAppUrl::to('admin/race_course/list.php')));?></td>
-        <td>競馬場データを管理します</td>
+        <td><?=(new MkTagA("テーマ設定",InAppUrl::to('admin/themes/list.php')));?></td>
+        <td>配色CSSファイルやテンプレートの保存先を作成・管理します</td>
+    </tr>
+    <tr>
+        <th colspan="2">競走馬関連マスタ管理</th>
     </tr>
     <tr>
         <td><?=(new MkTagA("所属マスタ管理",InAppUrl::to('admin/affiliation/list.php')));?></td>
@@ -69,11 +75,22 @@ if(!$session->is_logined()){
         <td></td>
     </tr>
     <tr>
-        <td colspan="2"></td>
+        <td><?=(new MkTagA("レース特殊結果マスタ設定",InAppUrl::to('admin/race_special_results/list.php')));?></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th colspan="2">レース情報関連マスタ</th>
+    </tr>
+    <tr>
+        <td><?=(new MkTagA("競馬場マスタ管理",InAppUrl::to('admin/race_course/list.php')));?></td>
+        <td>競馬場データを管理します</td>
     </tr>
     <tr>
         <td><?=(new MkTagA("レース格付マスタ管理",InAppUrl::to('admin/race_grade/list.php')));?></td>
         <td></td>
+    </tr>
+    <tr>
+        <td colspan="2"></td>
     </tr>
     <tr>
         <td><?=(new MkTagA("馬齢条件マスタ管理",InAppUrl::to('admin/race_category_age/list.php')));?></td>
@@ -86,17 +103,6 @@ if(!$session->is_logined()){
     <tr>
         <td><?=(new MkTagA("レース週マスタ設定",InAppUrl::to('admin/race_week/list.php')));?></td>
         <td>正確な日付無しのレース開催週の設定を調整します</td>
-    </tr>
-    <tr>
-        <td><?=(new MkTagA("レース特殊結果マスタ設定",InAppUrl::to('admin/race_special_results/list.php')));?></td>
-        <td></td>
-    </tr>
-    <tr>
-        <td colspan="2"></td>
-    </tr>
-    <tr>
-        <td><?=(new MkTagA("テーマ設定",InAppUrl::to('admin/themes/list.php')));?></td>
-        <td>配色CSSファイルの保存先を作成・管理します</td>
     </tr>
 </table>
 <hr class="no-css-fallback">
