4798f0068cd66b627cd3dceca14564f808542c10
polaris80red <polaris8000red@gmail.com>
2025-09-19 22:39:19 +0900

    add: レース特殊結果マスタの処理を共通化して簡易的なバリデーションを追加

--- libs/Class/table/Row/RaceSpecialResultsRow.php
+++ libs/Class/table/Row/RaceSpecialResultsRow.php
@@ -1,5 +1,6 @@
 <?php
 class RaceSpecialResultsRow extends TableRow {
+    use TableRowValidate;
     public const INT_COLUMNS=[
         'id',
         'is_registration_only',
@@ -20,4 +21,17 @@ class RaceSpecialResultsRow extends TableRow {
     public $is_excluded_from_race_count;
     public $sort_number;
     public $is_enabled=1;
+    /**
+     * SET済みのプロパティを検証して$hasErrorsをセットする
+     * @return bool エラーがなければtrue
+     */
+    public function validate(): bool
+    {
+        $this->validateRequired($this->unique_name,'キー名称');
+        $this->validateStrLength($this->unique_name,'キー名称',8);
+        $this->validateStrLength($this->name,'名称',50);
+        $this->validateStrLength($this->short_name_2,'2字略名',2);
+        $this->varidateInt($this->sort_number,'表示順補正');
+        return !$this->hasErrors;
+    }
 }
