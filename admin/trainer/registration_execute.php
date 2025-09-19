61a209e6234a3794504d13b25d5552167c9f5563
polaris80red <polaris8000red@gmail.com>
2025-09-19 22:21:06 +0900

    update: 調教師マスタの処理を共通化して簡易的なバリデーションを追加

--- admin/trainer/registration_execute.php
+++ admin/trainer/registration_execute.php
@@ -14,14 +14,18 @@ if(!Session::is_logined()){ $page->exitToHome(); }
 $pdo=getPDO();
 $id=filter_input(INPUT_POST,'id',FILTER_VALIDATE_INT);
 
+$editMode=($id>0);
 $TableClass=Trainer::class;
 $TableRowClass=$TableClass::ROW_CLASS;
 
-$form_item=new ($TableRowClass)();
-$check_form_item=false;
-if($id>0){
-    $check_form_item=($TableClass)::getById($pdo,$id);
-    $form_item->id=$id;
+if($editMode){
+    $page->title.="（編集）";
+    $form_item=($TableClass)::getById($pdo,$id);
+    if($form_item===false){
+        $page->addErrorMsg("ID '{$id}' が指定されていますが該当するレコードがありません");
+    }
+}else{
+    $form_item=new ($TableRowClass)();
 }
 $form_item->unique_name=(string)filter_input(INPUT_POST,'unique_name');
 $form_item->name=filter_input(INPUT_POST,'name');
@@ -29,29 +33,21 @@ $form_item->short_name_10=filter_input(INPUT_POST,'short_name_10');
 $form_item->affiliation_name=filter_input(INPUT_POST,'affiliation_name');
 $form_item->is_anonymous=filter_input(INPUT_POST,'is_anonymous',FILTER_VALIDATE_BOOL)?1:0;
 $form_item->is_enabled=filter_input(INPUT_POST,'is_enabled',FILTER_VALIDATE_BOOL)?1:0;
-
 do{
     if(!(new FormCsrfToken())->isValid()){
         ELog::error($page->title.": CSRFトークンエラー|".__FILE__);
         $page->addErrorMsg("登録編集フォームまで戻り、内容確認からやりなおしてください（CSRFトークンエラー）");
         break;
     }
-    if($form_item->unique_name==''){
-        $page->addErrorMsg("キー名が入力されていません");
-    }
-    if(mb_strlen($form_item->short_name_10,'UTF-8')>10){
-        $page->addErrorMsg("略名は10文字以内で設定してください");
-    }
-    if($id>0 && $check_form_item===false){
-        $page->debug_dump_var[]=['POST'=>$_POST];
-        $page->addErrorMsg("{$base_title}設定ID '{$id}' が指定されていますが該当する{$base_title}がありません");
+    if(!$form_item->validate()){
+        $page->addErrorMsgArray($form_item->errorMessages);
     }
 }while(false);
 if($page->error_exists){
     $page->printCommonErrorPage();
     exit;
 }
-if($check_form_item!=false){
+if($editMode){
     // 編集モード
     $result = ($TableClass)::UpdateFromRowObj($pdo,$form_item);
     if($result){
