42b2537c1a68bd955d55d0d353f1c1c3c0f2389d
polaris80red <polaris8000red@gmail.com>
2025-09-19 21:05:54 +0900

    add: ワールド管理にバリデーションを追加

--- libs/Class/table/Row/WorldRow.php
+++ libs/Class/table/Row/WorldRow.php
@@ -1,5 +1,6 @@
 <?php
 class WorldRow extends TableRow {
+    use TableRowValidate;
     public const STR_COLUMNS=[
         'name',
         'auto_id_prefix',
@@ -18,4 +19,13 @@ class WorldRow extends TableRow {
     public $sort_priority=0;
     public $sort_number=null;
     public $is_enabled=1;
+    public function validate(): bool
+    {
+        $this->validateRequired($this->name,'名称');
+        $this->validateStrLength($this->name,'名称',50);
+        $this->varidateInt($this->sort_priority,'表示順優先度');
+        $this->varidateInt($this->sort_number,'表示順補正');
+        $this->validateStrLength($this->auto_id_prefix,'自動ID接頭辞',30);
+        return !$this->hasErrors;
+    }
 }
