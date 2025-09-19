a75b56f8729290df28cfa48d7616e33aed70336c
polaris80red <polaris8000red@gmail.com>
2025-09-19 23:12:17 +0900

    update: 性別条件マスタの処理を共通化して簡易的なバリデーションを追加

--- libs/Class/table/Row/RaceCategorySexRow.php
+++ libs/Class/table/Row/RaceCategorySexRow.php
@@ -1,5 +1,6 @@
 <?php
 class RaceCategorySexRow extends TableRow {
+    use TableRowValidate;
     public const INT_COLUMNS=[
         'id',
         'sort_number',
@@ -16,4 +17,16 @@ class RaceCategorySexRow extends TableRow {
     public $umm_category;
     public $sort_number;
     public $is_enabled=1;
+    /**
+     * SET済みのプロパティを検証して$hasErrorsをセットする
+     * @return bool エラーがなければtrue
+     */
+    public function validate(): bool
+    {
+        $this->validateStrLength($this->name,'名称',50);
+        $this->validateStrLength($this->short_name_3,'3字略名',3);
+        $this->validateStrLength($this->umm_category,'擬人化用名称',16);
+        $this->varidateInt($this->sort_number,'表示順補正');
+        return !$this->hasErrors;
+    }
 }
