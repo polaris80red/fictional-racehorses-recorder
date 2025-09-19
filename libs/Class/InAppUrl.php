a68f154016edfb29d4e1fafb5f58849f88ffbb19
polaris80red <polaris8000red@gmail.com>
2025-09-19 19:41:20 +0900

    update: 競走馬詳細戦績の競走馬別レース前後コメント行からのコメント編集画面は該当の馬の行にスクロールするように

--- libs/Class/InAppUrl.php
+++ libs/Class/InAppUrl.php
@@ -4,10 +4,13 @@ class InAppUrl {
     public static function init(int $depth = 0) {
         self::$basePath = $depth > 0 ? str_repeat('../',$depth) : './';
     }
-    public static function to(string $path='', array $params=[]){
+    public static function to(string $path='', array $params=[],string $scroll_id=''){
         $url=self::$basePath.ltrim($path,'/');
         if($params!==[]){
             $url.="?".http_build_query($params);
+        }
+        if($scroll_id!==''){
+            $url.="#".ltrim($scroll_id,"#");
         }
         return $url;
     }
