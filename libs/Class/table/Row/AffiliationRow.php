17eee0eac1ba02a6b1a858cc39f4676008204591
polaris80red <polaris8000red@gmail.com>
2025-09-19 22:47:18 +0900

    add: 所属マスタの処理を共通化して簡易的なバリデーションを追加

--- libs/Class/table/Row/AffiliationRow.php
+++ libs/Class/table/Row/AffiliationRow.php
@@ -1,5 +1,6 @@
 <?php
 class AffiliationRow extends TableRow {
+    use TableRowValidate;
     public const INT_COLUMNS=[
         'id',
         'sort_number',
@@ -14,4 +15,15 @@ class AffiliationRow extends TableRow {
     public $sort_number;
     public $show_in_select_box=1;
     public $is_enabled=1;
+    /**
+     * SET済みのプロパティを検証して$hasErrorsをセットする
+     * @return bool エラーがなければtrue
+     */
+    public function validate(): bool
+    {
+        $this->validateRequired($this->unique_name,'キー名称');
+        $this->validateStrLength($this->unique_name,'キー名称',10);
+        $this->varidateInt($this->sort_number,'表示順補正');
+        return !$this->hasErrors;
+    }
 }
