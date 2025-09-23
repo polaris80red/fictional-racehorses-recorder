47802a629f52d007eeb836b609ed782cad92eec1
polaris80red <polaris8000red@gmail.com>
2025-09-23 21:53:32 +0900

    add: ユーザー管理にcreated_by・updated_byを実装

--- libs/Class/User.php
+++ libs/Class/User.php
@@ -3,7 +3,11 @@
  * ユーザークラス
  */
 class User {
+    private int|null $id=null;
     private bool $isSuperAdmin=false;
+    public function __construct(int|null $id=null){
+        $this->id=$id;
+    }
     /**
      * 固定管理者の場合のインスタンス生成
      */
@@ -11,5 +15,8 @@ class User {
         $user = new self();
         $user->isSuperAdmin=true;
         return $user;
+    }
+    public function getId(){
+        return $this->id;
     }
 }
