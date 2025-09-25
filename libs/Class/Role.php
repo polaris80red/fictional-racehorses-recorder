68e0cde9936810ad8bc31d3b31ac38e8cb08688b
polaris80red <polaris8000red@gmail.com>
2025-09-25 19:52:35 +0900

    add: ユーザー管理フォームに権限内容の説明表を追加

--- libs/Class/Role.php
+++ libs/Class/Role.php
@@ -11,15 +11,19 @@ class Role {
     const RoleInfoList=[
         self::Administrator=>[
             'name'=>'管理者',
+            'caption'=>"管理者",
         ],
         self::Maintainer=>[
             'name'=>'メンテナ',
+            'caption'=>"マスタ管理が可能",
         ],
         self::Editor=>[
             'name'=>'編集者',
+            'caption'=>"他ユーザーが登録したレコードも管理可能",
         ],
         self::Author=>[
             'name'=>'投稿者',
+            'caption'=>"自分が登録した馬とその戦績、および自分が登録したレースのみ編集可。レース削除不可",
         ],
     ];
 }
