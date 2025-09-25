e097e88c5038b099077af1c1b18a9af374f98ea8
polaris80red <polaris8000red@gmail.com>
2025-09-25 19:50:53 +0900

    add: 騎手マスタの管理にマスタ権限の判定を追加

--- admin/jockey/update_unique_name/confirm.php
+++ admin/jockey/update_unique_name/confirm.php
@@ -9,7 +9,10 @@ $page->title="騎手マスタ管理｜キー名称の一括変更：確認";
 $page->ForceNoindex();
 $session=new Session();
 if(!Session::isLoggedIn()){ $page->exitToHome(); }
-
+if(!Session::currentUser()->canManageMaster()){
+    header("HTTP/1.1 403 Forbidden");
+    $page->addErrorMsg('マスタ管理権限がありません');
+}
 $u_name=(string)filter_input(INPUT_POST,'u_name');
 $new_unique_name=trim((string)filter_input(INPUT_POST,'new_unique_name'));
 
