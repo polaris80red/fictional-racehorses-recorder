4657e2d8e6027346ac18977b87c8cbd0453ff3d7
polaris80red <polaris8000red@gmail.com>
2025-09-26 19:29:01 +0900

    change: 競走馬削除の判定を削除の権限に分離

--- horse/manage/delete/execute.php
+++ horse/manage/delete/execute.php
@@ -25,9 +25,9 @@ do{
         $page->addErrorMsg("入力元ID：{$horse_id}");
         break;
     }
-    if($horse && !Session::currentUser()->canHorseEdit($horse)){
+    if($horse && !Session::currentUser()->canDeleteHorse($horse)){
         header("HTTP/1.1 403 Forbidden");
-        $page->addErrorMsg("編集権限がありません");
+        $page->addErrorMsg("削除権限がありません");
         break;
     }
 }while(false);
