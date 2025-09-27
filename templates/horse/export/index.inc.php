007ed1a0159fdd6a2e64e9634a1edc2db155c25d
polaris80red <polaris8000red@gmail.com>
2025-09-27 21:10:59 +0900

    change: 競走馬情報エクスポートの一覧部分のテンプレートをより改造サンプルにしやすいよう変数指定などを変更

--- templates/horse/export/index.inc.php
+++ templates/horse/export/index.inc.php
@@ -1,8 +1,20 @@
+<?php
+/**
+ * 競走馬情報エクスポート画面のメインページの内容部分
+ * @var HorseRow $horse
+ */
+?>
 <ul>
     <li>戦績｜スプレッドシート・Excel用タブ区切りテキスト
         <ul>
-            <li><?=(new MkTagA('戦績TSV昇順','./history_tsv.php?'.$urlparam.'&horse_history_order=asc'))?></li>
-            <li><?=(new MkTagA('戦績TSV降順','./history_tsv.php?'.$urlparam.'&horse_history_order=desc'))?></li>
+            <li><?=(new MkTagA('戦績TSV昇順','./history_tsv.php?'.http_build_query([
+                'horse_id'=>$horse->horse_id,
+                'horse_history_order'=>'asc'
+                ])))?></li>
+            <li><?=(new MkTagA('戦績TSV降順','./history_tsv.php?'.http_build_query([
+                'horse_id'=>$horse->horse_id,
+                'horse_history_order'=>'desc'
+                ])))?></li>
         </ul>
     </li>
 </ul>
