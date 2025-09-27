0a481ed9dbdd97e14d8dc459b6f4a9e31dbcc9a8
polaris80red <polaris8000red@gmail.com>
2025-09-27 20:58:57 +0900

    add: 競走馬情報エクスポートページに戦績の昇順・降順モードを分岐（パラメータ分岐をテンプレートに入れてカスタマイズするサンプル）

--- templates/horse/export/index.inc.php
+++ templates/horse/export/index.inc.php
@@ -1,3 +1,8 @@
 <ul>
-    <li><?=(new MkTagA('戦績｜スプレッドシート・Excel用タブ区切りテキスト','./history_tsv.php?'.$urlparam))?></li>
+    <li>戦績｜スプレッドシート・Excel用タブ区切りテキスト
+        <ul>
+            <li><?=(new MkTagA('戦績TSV昇順','./history_tsv.php?'.$urlparam.'&horse_history_order=asc'))?></li>
+            <li><?=(new MkTagA('戦績TSV降順','./history_tsv.php?'.$urlparam.'&horse_history_order=desc'))?></li>
+        </ul>
+    </li>
 </ul>
