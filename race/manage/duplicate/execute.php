ea9c687457aabacecf21470b1758991c3c1ff677
polaris80red <polaris8000red@gmail.com>
2025-09-29 23:43:18 +0900

    change: レース一括複写のtitle関連を新形式に統一

--- race/manage/duplicate/execute.php
+++ race/manage/duplicate/execute.php
@@ -65,7 +65,7 @@ $world=World::getById($pdo,$world_id);
 ?><!DOCTYPE html>
 <html lang="ja">
 <head>
-    <title><?php $page->printTitle();?></title>
+    <title><?=h($page->renderTitle())?></title>
     <meta charset="UTF-8">
     <meta http-equiv="content-language" content="ja">
     <?=$page->getMetaNoindex()?>
@@ -83,7 +83,7 @@ th{ background-color: #EEE;}
 <body>
 <header>
 <?php $page->printHeaderNavigation(); ?>
-<h1 class="page_title"><?php $page->printTitle(); ?></h1>
+<h1 class="page_title"><?=h($page->title)?></h1>
 </header>
 <main id="content">
 <hr class="no-css-fallback">
