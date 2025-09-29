70d51226db69caf917a4c3ab5046a15350b5c101
polaris80red <polaris8000red@gmail.com>
2025-09-29 23:20:44 +0900

    change: パスワード変更画面のtitle関連を新形式に統一

--- account/registration_execute.php
+++ account/registration_execute.php
@@ -46,7 +46,7 @@ $result = ($TableClass)::UpdateFromRowObj($pdo,$form_item);
 ?><!DOCTYPE html>
 <html lang="ja">
 <head>
-    <title><?php $page->printTitle();  ?></title>
+    <title><?=h($page->renderTitle())?></title>
     <meta charset="UTF-8">
     <meta http-equiv="content-language" content="ja">
     <?=$page->getMetaNoindex()?>
@@ -57,7 +57,7 @@ $result = ($TableClass)::UpdateFromRowObj($pdo,$form_item);
 <body>
 <header>
 <?php $page->printHeaderNavigation(); ?>
-<h1 class="page_title"><?php $page->printTitle(); ?></h1>
+<h1 class="page_title"><?=h($page->title)?></h1>
 </header>
 <main id="content">
 <hr class="no-css-fallback">
